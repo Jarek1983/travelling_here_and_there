ee01fd56cee163dd1a45af8c1a1b0fbd23f30f40
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-12-02 09:44:16 +0100

    Move find article to dedicated method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
   def index
   	# binding.pry
     @articles = Article.all.order(id: :desc)
@@ -22,16 +22,18 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
+    # find_article
   end
 
   def edit
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
   end
 
   def update
   	# article_params = params.require(:article).permit(:title, :text)
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
+    # find_article
 	  if @article.update(article_params)
 	    redirect_to article_path(@article)
 	  else
@@ -40,7 +42,8 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
+    # find_article
     @article.destroy
     redirect_to articles_path
   end
@@ -49,6 +52,10 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title,:text)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 
 end
