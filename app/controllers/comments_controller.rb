3035602728627351d99a5ff0ce7ffb514161f74a
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-12-02 12:17:27 +0100

    Change routes, _form.html.erb for comments, comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :find_article
 
 	def create
-		  
+		  # binding.pry
 		  @comment = Comment.new(comment_params)
+		  @comment.article = @article
 		  @comment.save
-		  redirect_to article_path(@comment.article)
+
+		  redirect_to article_path(@article)
 	end
 
-private
+    private
+
+    def find_article
+    	@article = Article.find(params[:article_id])
 
 	def comment_params
 		params.require(:comment).permit(:commenter, :body, :article_id)
 	end		
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
   # patch 'articles/:id', to: 'articles#update'
   # delete 'articles/:id', to: 'articles#destroy'
 
-  resources :articles 
-  resources :comments
-  
+  resources :articles do
+    resources :comments
+  end
     
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
